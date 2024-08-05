513441f2bf49f91fa029e6e4b4f7fef161765192
Amit Sharma <amitsha9634@gmail.com>
2024-08-05 15:00:15 +0000

    vite

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom/client'
 import { render } from 'preact'
 import { App } from './app.jsx'
 
+function MyApp() {
+    return(
+        <div>
+           <h1>
+            Custom App !
+           </h1>
+        </div>
+    )
+}
 
-render(<App />, document.getElementById('app')).
+/* const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://goggle.com',
+        target: '_blank'
+    } ,
+    children: 'Click me to visit goggle'
+} */
+
+const anotherElement = (
+    <a href="https://goggle.com" target='_blank'>Visit goggle</a>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).
+
 render(
     <App />
 )
