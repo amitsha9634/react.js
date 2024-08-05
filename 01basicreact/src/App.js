ed8fbeef644c692b7573fed62e85258a845e112e
Amit Sharma <amitsha9634@gmail.com>
2024-08-05 06:27:43 +0000

    basic-react

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,12 @@
 
-
+import Chai from "./Chai"
 
 function App() {
   return (
+    <>
+    <Chai />
    <h1>hello world</h1>
+   </>
   );
 }
 
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,10 @@
+
+
+
+function Chai() {
+    return(
+        <h2>Chai or react</h2>
+    )
+}
+
+export default Chai
