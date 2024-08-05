3257ee0a18b741c71f04e3fb169e74f776dc331f
Amit Sharma <amitsha9634@gmail.com>
2024-08-05 14:58:59 +0000

    custom-react

---
+++ customReact/custom.js
@@ -0,0 +1,23 @@
+function customrendor(reactElement, container) {
+    const domElement = document.createElement
+    (reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+}
+
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://goggle.com',
+        target: '_blank'
+    } ,
+    children: 'Click me to visit goggle'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customrendor(reactElement, mainContainer)
