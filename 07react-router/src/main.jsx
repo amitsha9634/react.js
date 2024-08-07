136a0e6c8ec84f68d842e46d51d42f472a68f995
Amit Sharma <amitsha9634@gmail.com>
2024-08-07 09:15:56 +0000

    07react-router

--- 07react-router/src/main.jsx
+++ 07react-router/src/main.jsx
@@ -5,8 +5,9 @@ import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
-import About from './components/About/About.jsx'
+import About from './components/About /About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
 import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 
@@ -36,8 +37,8 @@ import Github, { githubInfoLoader } from './components/Github/Github.jsx'
     createRoutesFromElements(
       <Route path='/' element={<Layout />}>
         <Route path='' element={<Home />} />
-        <Route path='' element={<About />} />
-        <Route path='' element={<Contact />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
         <Route path='user/:userid' element={<User />} />
         <Route
          loader={githubInfoLoader}
