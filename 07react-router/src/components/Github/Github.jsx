aac86aa920227cb6c09d9adfb44ce1c87f5ff0ef
Amit Sharma <amitsha9634@gmail.com>
2024-08-07 09:02:21 +0000

    07react-router

---
+++ 07react-router/src/components/Github/Github.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+   /*
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('https://api.github.com/users/amitsha9634')
+        .then(response => response.jdon())
+        .then(data => {
+            console.log(data); 
+        })
+    }, [])
+    */
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github followers: {data.followers}
+      <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/amitsha9634')
+    return response.json()
+}
